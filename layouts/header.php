a533875100291e563e423a4fca297cc3483da9e0
javims95 <javier_ms95@hotmail.com>
2020-11-24 09:07:32 +0100

    Nav-admin (working)

--- layouts/header.php
+++ layouts/header.php
@@ -1,53 +1,79 @@
 <?php
 include 'php\visitas.php';
-
-
-
 ?>
 
 <head>
   <style>
-    @media only screen and (max-width: 992px){
-      .ocultarMenuAdmin{
+    @media only screen and (max-width: 992px) {
+      .ocultarMenuAdmin {
         display: none;
       }
+    }
+
+    a.gear-nav {
+      color: white !important;
     }
   </style>
 </head>
 <header class="site-navbar" role="banner">
 
   <!-- Menú de administración -->
-<nav class="navbar navbar-expand-lg navbar-light navbar navbar-dark bg-dark ocultarMenuAdmin" style="height: 40px;">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+  <nav class="navbar navbar-expand-lg navbar-light navbar navbar-dark bg-dark ocultarMenuAdmin" style="height: 40px;">
+    <a class="gear-nav" href="#"><span class="icon icon-cogs"></span></a>
+    <a style="margin-left: 10px;" class="navbar-brand" href="admin">Administración</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Tienda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="admin\pedidos.php">Pedidos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Productos
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="admin\productos.php">Ver Todos</a>
+            <a class="dropdown-item" href="admin\nuevoProducto.php">Añadir Nuevo</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="admin\pedidos.php">Cupones</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Usuarios
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Ver Todos</a>
+            <a class="dropdown-item" href="#">Añadir Nuevo</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="admin\mailBox.php">Mensajes</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Exportar
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">JSON</a>
+            <a class="dropdown-item" href="#">XML</a>
+            <a class="dropdown-item" href="#">CSV</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="php\cerrarSesion.php">Cerrar Sesión</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-<!-- Menú Principal -->
+  <!-- Menú Principal -->
   <div class="site-navbar-top">
     <div class="container">
       <div class="row align-items-center">
