59cc6233c486f7f3d81f10f8880f6a362cbc5ab1
javims95 <javier_ms95@hotmail.com>
2021-02-04 20:20:13 +0100

    Menú opciones

--- admin/partials/panther-admin-display.php
+++ admin/partials/panther-admin-display.php
@@ -1,31 +1,49 @@
 <div class="row">
-    <div class="col-12">
-        <div class="row">
-            <div class="col-12">
+    <div class="col-12 pl-0">
+        <div class="row position-relative mb-0">
 
-                <?php 
-                $panther_img = PANTHER_DIR_URI . 'admin/img/jungla.jpg';
+        <!-- Botón guardar cambios -->
+        <button class="btn btn-success btn-panther"><i class="fas fa-save"></i> Guardar</button>
+
+            <div class="col-12 pl-0">                
+                <?php
                 $bg_gradient = "
-                background: rgba(210,255,82,1), url($panther_img);
-                background: -moz-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(81,121,51,1) 100%), url($panther_img);
-                background: -webkit-gradient(left top, right top, color-stop(0%, rgba(210,255,82,1)), color-stop(100%, rgba(81,121,51,1))), url($panther_img);
-                background: -webkit-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(81,121,51,1) 100%), url($panther_img);
-                background: -o-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(81,121,51,1) 100%), url($panther_img);
-                background: -ms-linear-gradient(left, rgba(210,255,82,1) 0%, rgba(81,121,51,1) 100%), url($panther_img);
-                background: linear-gradient(to right, rgba(210,255,82,1) 0%, rgba(81,121,51,1) 100%), url($panther_img);
-                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#d2ff52', endColorstr='#517933', GradientType=1 );
+                background: rgb(42,34,195);
+                background: -moz-linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
+                background: -webkit-linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
+                background: linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
+                filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#2a22c3',endColorstr='#882dfd',GradientType=1);
                 ";
                 ?>
 
-                <div class="panther-header" style="<?php echo $bg_gradient; ?>">
+                <div class="panther-header pt-4" style="<?php echo $bg_gradient; ?>">
                     <div class="panther-port">
                         <img src="<?php echo PANTHER_DIR_URI; ?>admin/img/panther.svg" alt="">
                     </div>
                     <div class="panther-header-title">
                         <h4><i class="fas fa-cog"></i> <span>Opciones</span></h4>
-                    </div>
+                    </div>                    
                 </div>
             </div>
         </div>
+
+        <!-- Columna de navegación opciones tema -->
+        <div class="row ml-1">
+            <div class="col-3">
+                <ul class="panther-menu">
+                    <li>
+                        <a href="#panther-global" class="active" data-menuActivado="global">
+                        <i class="fas fa-cog"></i><span> Opciones</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#panther-global" data-menuActivado="global">
+                        <i class="fas fa-cog"></i><span> Opciones</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
     </div>
 </div>
