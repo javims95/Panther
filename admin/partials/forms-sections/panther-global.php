7b5eedd5b6c2034ba67ab50cbc65418e76935cea
javims95 <javier_ms95@hotmail.com>
2021-02-05 08:42:06 +0100

    Pagina configuracion del tema

---
+++ admin/partials/forms-sections/panther-global.php
@@ -0,0 +1,50 @@
+<div id="panther-global" class="panther-content col-12">
+    <section id="panther-global-body" class="my-3">
+        <div class="panther-sec-title">
+            <h5>
+                <span><strong>Cuerpo</strong></span>
+            </h5>
+        </div>
+
+        <div class="panther-sec-configs">  
+
+            <!-- Input -->
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <h6><strong>Texto</strong></h6>
+                </div>
+                <div class="col-md-8">
+                    <input class="form-control" type="text" value="" id="">
+                </div>
+            </div>
+
+            <!-- Textarea -->
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <h6><strong>Texto</strong></h6>
+                </div>
+                <div class="col-md-8">
+                    <textarea name="" class="form-control"></textarea>
+                </div>
+            </div>
+
+            <!-- Input media -->
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <h6><strong>Texto</strong></h6>
+                </div>
+                <div class="col-md-8 panther-input-media d-flex">
+                <div class="form-group">
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    <!-- TERMINAR LA X, PARA ELIMINAR UN ARCHIVO AL CARGARLO -->
+                    <span><i class="fas fa-times"></i></span>
+                </div>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
--- admin/partials/panther-admin-display.php
+++ admin/partials/panther-admin-display.php
@@ -2,48 +2,60 @@
     <div class="col-12 pl-0">
         <div class="row position-relative mb-0">
 
-        <!-- Botón guardar cambios -->
-        <button class="btn btn-success btn-panther"><i class="fas fa-save"></i> Guardar</button>
+            <!-- Botón guardar cambios -->
+            <button class="btn btn-success btn-panther"><i class="fas fa-save"></i> Guardar</button>
 
-            <div class="col-12 pl-0">                
-                <?php
-                $bg_gradient = "
-                background: rgb(42,34,195);
-                background: -moz-linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
-                background: -webkit-linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
-                background: linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
-                filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#2a22c3',endColorstr='#882dfd',GradientType=1);
-                ";
-                ?>
+                <div class="col-12 pl-0">                
+                    <?php
+                    $bg_gradient = "
+                    background: rgb(42,34,195);
+                    background: -moz-linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
+                    background: -webkit-linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
+                    background: linear-gradient(0deg, rgba(42,34,195,1) 0%, rgba(136,45,253,1) 100%);
+                    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#2a22c3',endColorstr='#882dfd',GradientType=1);
+                    ";
+                    ?>
 
-                <div class="panther-header pt-4" style="<?php echo $bg_gradient; ?>">
-                    <div class="panther-port">
-                        <img src="<?php echo PANTHER_DIR_URI; ?>admin/img/panther.svg" alt="">
+                    <div class="panther-header pt-4" style="<?php echo $bg_gradient; ?>">
+                        <div class="panther-port">
+                            <img src="<?php echo PANTHER_DIR_URI; ?>admin/img/panther.svg" alt="">
+                        </div>
+                        <div class="panther-header-title">
+                            <h4><i class="fas fa-cog"></i> <span>Opciones</span></h4>
+                        </div>                    
                     </div>
-                    <div class="panther-header-title">
-                        <h4><i class="fas fa-cog"></i> <span>Opciones</span></h4>
-                    </div>                    
+                </div>
+            </div>
+
+            <!-- Columna de navegación opciones tema -->
+            <div class="row ml-1">
+                <div class="col-3">
+                    <ul class="panther-menu">
+                        <li>
+                            <a href="#panther-global" class="active" data-menuActivado="global">
+                            <i class="fas fa-cog"></i><span> Opciones</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#panther-global" data-menuActivado="global">
+                            <i class="fas fa-cog"></i><span> Opciones</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Contenido de la configuracion -->
+                <div class="col-9">
+                    <form id="panther-form">
+                        <div id="panther-form-content">
+                            <input type="hidden" name="panther[menuActivado]" id="menuActivado">
+
+                            <!-- Configuraciones globales -->
+                            <?php require_once PANTHER_DIR_PATH . 'admin/partials/forms-sections/panther-global.php'; ?>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-
-        <!-- Columna de navegación opciones tema -->
-        <div class="row ml-1">
-            <div class="col-3">
-                <ul class="panther-menu">
-                    <li>
-                        <a href="#panther-global" class="active" data-menuActivado="global">
-                        <i class="fas fa-cog"></i><span> Opciones</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#panther-global" data-menuActivado="global">
-                        <i class="fas fa-cog"></i><span> Opciones</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
     </div>
 </div>
